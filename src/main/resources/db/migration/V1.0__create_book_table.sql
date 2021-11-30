e19b4537b66c5854b4f568c5737a2977630027dc
Saeid Babaei <babaei503@gmail.com>
2021-11-30 22:41:13 +0330

    Add Flyway for database migration

---
+++ src/main/resources/db/migration/V1.0__create_book_table.sql
@@ -0,0 +1,8 @@
+    Create Table IF NOT EXISTS book (
+       book_id binary(255) not null,
+        active bit not null,
+        author varchar(255),
+        price decimal(19,2) not null,
+        title varchar(255) not null,
+        primary key (book_id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
